5ffa14497afd1463077b1aec4d0fecc7d0bba5a1
Courtney Larson <larso126@msu.edu>
2017-05-26 14:38:01 -0400

    Ordination and PERMANOVA

--- Hunt_Creek_16S.R
+++ Hunt_Creek_16S.R
@@ -4,10 +4,15 @@
 library(reshape)
 library(ggplot2)
 library(vegan)
+library(plyr)
+library(dplyr)
 
 #color vectors
 two_col_vec <- c("black", "bisque3")
+three_col_vec<- c("#a6cee3", "#1f78b4", "#b2df8a")
 five_col_vec<- c("#7fc97f", "#beaed4", "#fdc086", "#ffff99", "#386cb0")
+six_col_vec<- c("#e41a1c", "#377eb8", "green", "#984ea3", "#ff7f00", "#ffff33")
+six_col_vec_cont<-c("#fee5d9", "#fcbba1", "#fc9272", "#fb6a4a", "#de2d26", "#a50f15")
 
 #Get tables from different runs - R1, R2, R3, R4, R5 and R6
 Hunt_Creek_16S_R1<-read.table("~/Documents/MSU/Research/Hunt_Creek_Salmon/Microbes/Taxonomy_txt_tables/HC_R1_table_tabseparated.txt", sep="\t", header = T)
@@ -73,7 +78,10 @@ HC_16S_OTU_map_C_v<-HC_16S_OTU_map_C[, colSums(HC_16S_OTU_map_C != 0) > 0]
 #Make data table for just insect samples
 HC_16S_OTU_map_I<-subset(HC_16S_OTU_map, Source!="Carcass" & Source!="Biofilm")
 HC_16S_OTU_map_I$Source
-HC_16S_OTU_map_I_v<-HC_16S_OTU_map_I[, colSums(HC_16S_OTU_map_I != 0) > 0]
+
+#table for carcass and biofilms before and after
+HC_16S_OTU_map$Date<-as.factor(HC_16S_OTU_map$Date)
+HC_16S_OTU_map_CBF<-subset(HC_16S_OTU_map, Total_Biofilm_Growth_PostCarcass<=16 & Reach=="Salmon" & Source!="Rhyacophila" & Source!="Baetis" & Source!="Stegopterna")
 
 #Create melted file (this step takes a while, so prepare to wait. Skip if just doing community analysis like NMDS)
 HC_16S_OTU_map_m<-melt(HC_16S_OTU_map, id=c("Row.names"))
@@ -110,6 +118,9 @@ HC_16S_com_C<-as.matrix(HC_16S_OTU_map_C[,15:ncol(HC_16S_OTU_map_C)])
 #insect community data matrix
 HC_16S_com_I<-as.matrix(HC_16S_OTU_map_I[,15:ncol(HC_16S_OTU_map_I)])
 
+#carcass/b/a community data matrix
+HC_16S_com_CBF<-as.matrix(HC_16S_OTU_map_CBF[,15:ncol(HC_16S_OTU_map_CBF)])
+
 #Create overall environmental data matrix for community analysis
 HC_16S_env<-(HC_16S_OTU_map[,1:14])
 
@@ -122,56 +133,162 @@ HC_16S_env_C<-(HC_16S_OTU_map_C[,1:14])
 #Insect environmental data matrix
 HC_16S_env_I<-(HC_16S_OTU_map_I[,1:14])
 
+#carcassba env data matrix
+HC_16S_env_CBF<-(HC_16S_OTU_map_CBF[,1:14])
+
 #Make sample type variable for overall analysis
 Sample.type<-factor(HC_16S_env$Source)
 
+#Make reach variable for analysis
+stream_site=factor(HC_16S_env$Reach, c("Salmon", "Control"))
+
+#Make year variable for analysis
+Study_Year<-factor(HC_16S_env$Year, c("1", "2"))
+
+#Make days since carcass introduction variable for analysis and combine similar values for simplicity
+HC_16S_env$Total_Biofilm_Growth_PostCarcass<-as.factor(HC_16S_env$Total_Biofilm_Growth_PostCarcass)
+levels(HC_16S_env$Total_Biofilm_Growth_PostCarcass)
+HC_16S_env$Total_Biofilm_Growth_PostCarcass<-as.factor(gsub("16","14",HC_16S_env$Total_Biofilm_Growth_PostCarcass))
+HC_16S_env$Total_Biofilm_Growth_PostCarcass<-as.factor(gsub("141","142",HC_16S_env$Total_Biofilm_Growth_PostCarcass))
+HC_16S_env$Total_Biofilm_Growth_PostCarcass<-as.factor(gsub("265","264",HC_16S_env$Total_Biofilm_Growth_PostCarcass))
+Days_Carcass<-factor(HC_16S_env$Total_Biofilm_Growth_PostCarcass, c("0", "14", "162", "222", "264", "306"))
+
 #Make stream_site variable for biofilm analysis
-stream_site=factor(HC_16S_env_BF$Reach, c("Salmon", "Control"))
+stream_site_bf=factor(HC_16S_env_BF$Reach, c("Salmon", "Control"))
 
 #Make Year variable for biofilm analysis
-Study_Year<-factor(HC_16S_env_BF$Year, c("1", "2"))
+Study_Year_bf<-factor(HC_16S_env_BF$Year, c("1", "2"))
+
+#variable carcass before after 
+HC_16S_env_CBF$before_carcass_after<-paste(HC_16S_env_CBF$Source, HC_16S_env_CBF$Total_Biofilm_Growth_PostCarcass)
+HC_16S_env_CBF$before_carcass_after<-gsub("Biofilm 14", "Biofilm After", HC_16S_env_CBF$before_carcass_after)
+HC_16S_env_CBF$before_carcass_after<-gsub("Biofilm 16", "Biofilm After", HC_16S_env_CBF$before_carcass_after)
+HC_16S_env_CBF$before_carcass_after<-gsub("Biofilm 0", "Biofilm Before", HC_16S_env_CBF$before_carcass_after)
+HC_16S_env_CBF$before_carcass_after<-gsub("Carcass 0", "Carcass", HC_16S_env_CBF$before_carcass_after)
+carcass<-factor(HC_16S_env_CBF$before_carcass_after)
+
+#variable for insect type
+Taxa_I<-factor(HC_16S_env_I$Source, c("Rhyacophila", "Stegopterna", "Baetis"))
+
+#variable for stream site
+stream_site_I<-factor(HC_16S_env_I$Reach, c("Salmon", "Control"))
+
+#Make days since carcass introduction variable for analysis and combine similar values for simplicity biofilm
+HC_16S_env_BF$Total_Biofilm_Growth_PostCarcass<-as.factor(HC_16S_env_BF$Total_Biofilm_Growth_PostCarcass)
+levels(HC_16S_env_BF$Total_Biofilm_Growth_PostCarcass)
+HC_16S_env_BF$Total_Biofilm_Growth_PostCarcass<-as.factor(gsub("16","14",HC_16S_env_BF$Total_Biofilm_Growth_PostCarcass))
+HC_16S_env_BF$Total_Biofilm_Growth_PostCarcass<-as.factor(gsub("141","142",HC_16S_env_BF$Total_Biofilm_Growth_PostCarcass))
+HC_16S_env_BF$Total_Biofilm_Growth_PostCarcass<-as.factor(gsub("265","264",HC_16S_env_BF$Total_Biofilm_Growth_PostCarcass))
+Days_Carcass_BF<-factor(HC_16S_env_BF$Total_Biofilm_Growth_PostCarcass, c("0", "14", "142", "222", "264", "306"))
 
 #Overall NMDS for all 16S samples
 HC_16S_NMDS<-metaMDS(HC_16S_com, distance="bray")
-ordiplot(HC_16S_NMDS, type="n", main="16S")
 
 #stressplot for overall nmds
 stressplot(HC_16S_NMDS)
 
-#make figure for overall 16S nmds by sample type (biofilm, carcass, insect)
+#make figure for overall 16S nmds by sample type (biofilm, carcass, insect) with ellipses
+ordiplot(HC_16S_NMDS, type="n", main="16S")
 with(HC_16S_NMDS, points(HC_16S_NMDS, display="sites", col=five_col_vec[Sample.type], pch=19, pt.bg=five_col_vec))
 with(HC_16S_NMDS, legend("topleft", legend=levels(Sample.type), bty="n", col=five_col_vec, pch=19, pt.bg=two_col_vec, cex=0.75))
+with(HC_16S_NMDS, ordiellipse(HC_16S_NMDS, Sample.type, kind="se", conf=0.95, lwd=2, col="#7fc97f", show.groups = "Baetis"))
+with(HC_16S_NMDS, ordiellipse(HC_16S_NMDS, Sample.type, kind="se", conf=0.95, lwd=2, col="#beaed4", show.groups = "Biofilm"))
+with(HC_16S_NMDS, ordiellipse(HC_16S_NMDS, Sample.type, kind="se", conf=0.95, lwd=2, col="#fdc086", show.groups = "Carcass"))
+with(HC_16S_NMDS, ordiellipse(HC_16S_NMDS, Sample.type, kind="se", conf=0.95, lwd=2, col="#ffff99", show.groups = "Rhyacophila"))
+with(HC_16S_NMDS, ordiellipse(HC_16S_NMDS, Sample.type, kind="se", conf=0.95, lwd=2, col="#386cb0", show.groups = "Stegopterna"))
+
+#make figure where different colors are different sample types and ellipses are different reaches
+ordiplot(HC_16S_NMDS, type="n", main="16S")
+with(HC_16S_NMDS, points(HC_16S_NMDS, display="sites", col=five_col_vec[Sample.type], pch=19, pt.bg=five_col_vec))
+with(HC_16S_NMDS, legend("topleft", legend=levels(Sample.type), bty="n", col=five_col_vec, pch=19, pt.bg=two_col_vec, cex=0.75))
+with(HC_16S_NMDS, ordiellipse(HC_16S_NMDS, stream_site, kind="se", conf=0.95, lwd=2, col="black", show.groups = "Salmon"))
+with(HC_16S_NMDS, ordiellipse(HC_16S_NMDS, stream_site, kind="se", conf=0.95, lwd=2, col="bisque3", show.groups = "Control"))
+
+#make figure where different colors are different sample types and ellipses are different years
+ordiplot(HC_16S_NMDS, type="n", main="16S")
+with(HC_16S_NMDS, points(HC_16S_NMDS, display="sites", col=five_col_vec[Sample.type], pch=19, pt.bg=five_col_vec))
+with(HC_16S_NMDS, legend("topleft", legend=levels(Sample.type), bty="n", col=five_col_vec, pch=19, pt.bg=two_col_vec, cex=0.75))
+with(HC_16S_NMDS, ordiellipse(HC_16S_NMDS, Study_Year, kind="se", conf=0.95, lwd=2, col="black", show.groups = "1"))
+with(HC_16S_NMDS, ordiellipse(HC_16S_NMDS, Study_Year, kind="se", conf=0.95, lwd=2, col="bisque", show.groups = "2"))
+
+#make figure where different colors are different sample types and ellipses are different months
+ordiplot(HC_16S_NMDS, type="n", main="16S")
+with(HC_16S_NMDS, points(HC_16S_NMDS, display="sites", col=five_col_vec[Sample.type], pch=19, pt.bg=five_col_vec))
+with(HC_16S_NMDS, legend("topleft", legend=levels(Sample.type), bty="n", col=five_col_vec, pch=19, pt.bg=two_col_vec, cex=0.75))
+with(HC_16S_NMDS, ordiellipse(HC_16S_NMDS, Days_Carcass, kind="se", conf=0.95, lwd=2, col="black", show.groups = "0"))
+with(HC_16S_NMDS, ordiellipse(HC_16S_NMDS, Days_Carcass, kind="se", conf=0.95, lwd=2, col="black", show.groups = "14"))
+with(HC_16S_NMDS, ordiellipse(HC_16S_NMDS, Days_Carcass, kind="se", conf=0.95, lwd=2, col="black", show.groups = "162"))
+with(HC_16S_NMDS, ordiellipse(HC_16S_NMDS, Days_Carcass, kind="se", conf=0.95, lwd=2, col="black", show.groups = "222"))
+with(HC_16S_NMDS, ordiellipse(HC_16S_NMDS, Days_Carcass, kind="se", conf=0.95, lwd=2, col="black", show.groups = "264"))
+with(HC_16S_NMDS, ordiellipse(HC_16S_NMDS, Days_Carcass, kind="se", conf=0.95, lwd=2, col="black", show.groups = "306"))
+
+#Overal nmds by year of study
+ordiplot(HC_16S_NMDS, type="n", main="16S by year")
+with(HC_16S_NMDS, points(HC_16S_NMDS, display="sites", col=two_col_vec[Study_Year], pch=19, pt.bg=two_col_vec))
+with(HC_16S_NMDS, legend("topleft", legend=levels(Study_Year), bty="n", col=two_col_vec, pch=19, pt.bg=two_col_vec))
+
+#overall nmds by time since carcass introduction
+levels(Days_Carcass)
+ordiplot(HC_16S_NMDS, type="n", main="16S by time")
+with(HC_16S_NMDS, points(HC_16S_NMDS, display="sites", col=six_col_vec_cont[Days_Carcass], pch=19, pt.bg=six_col_vec_cont))
+with(HC_16S_NMDS, legend("topleft", legend=levels(Days_Carcass), bty="n", col=six_col_vec_cont, pch=19, pt.bg=six_col_vec_cont, cex=0.75))
+
+#overall nmds by reach
+ordiplot(HC_16S_NMDS, type="n", main="16S by site")
+with(HC_16S_NMDS, points(HC_16S_NMDS, display="sites", col=two_col_vec[stream_site], pch=19, pt.bg=two_col_vec))
+with(HC_16S_NMDS, legend("topleft", legend=levels(stream_site), bty="n", col=two_col_vec, pch=19, pt.bg=two_col_vec))
 
 #Overall permanova
 adonis(HC_16S_com ~ Reach + Month + Year + Source + Reach*Month + Reach*Year + Reach+Source + Month*Year + Month*Source + Year*Source, data=HC_16S_env, permutations=999)
 
 #make NMDS Biofilms
 HC_16S_BF_NMDS<-metaMDS(HC_16S_com_BF, distance="bray")
-ordiplot(HC_16S_BF_NMDS, type="n", main="16S Biofilms")
 
 #Stressplot for biofilm nmds
 stressplot(HC_16S_BF_NMDS)
 
 #Biofilms With site points by reach
-with(HC_16S_BF_NMDS, points(HC_16S_BF_NMDS, display="sites", col=two_col_vec[stream_site], pch=19, pt.bg=two_col_vec))
-with(HC_16S_BF_NMDS, legend("topleft", legend=levels(stream_site), bty="n", col=two_col_vec, pch=19, pt.bg=two_col_vec))
-with(HC_16S_BF_NMDS, ordiellipse(HC_16S_BF_NMDS, stream_site, kind="se", conf=0.95, lwd=2, col="black", show.groups = "Salmon"))
-with(HC_16S_BF_NMDS, ordiellipse(HC_16S_BF_NMDS, stream_site, kind="se", conf=0.95, lwd=2, col="bisque3", show.groups = "Control"))
+ordiplot(HC_16S_BF_NMDS, type="n", main="16S Biofilms")
+with(HC_16S_BF_NMDS, points(HC_16S_BF_NMDS, display="sites", col=two_col_vec[stream_site_bf], pch=19, pt.bg=two_col_vec))
+with(HC_16S_BF_NMDS, legend("topleft", legend=levels(stream_site_bf), bty="n", col=two_col_vec, pch=19, pt.bg=two_col_vec))
+with(HC_16S_BF_NMDS, ordiellipse(HC_16S_BF_NMDS, stream_site_bf, kind="se", conf=0.95, lwd=2, col="black", show.groups = "Salmon"))
+with(HC_16S_BF_NMDS, ordiellipse(HC_16S_BF_NMDS, stream_site_bf, kind="se", conf=0.95, lwd=2, col="bisque3", show.groups = "Control"))
 
 #Biofilms with site points by year
+ordiplot(HC_16S_BF_NMDS, type="n", main="16S Biofilms Year")
 with(HC_16S_BF_NMDS, points(HC_16S_BF_NMDS, display="sites", col=two_col_vec[Study_Year], pch=19, pt.bg=two_col_vec))
 with(HC_16S_BF_NMDS, legend("topleft", legend=levels(Study_Year), bty="n", col=two_col_vec, pch=19, pt.bg=two_col_vec))
 with(HC_16S_BF_NMDS, ordiellipse(HC_16S_BF_NMDS, Study_Year, kind="se", conf=0.95, lwd=2, col="black", show.groups = "1"))
 with(HC_16S_BF_NMDS, ordiellipse(HC_16S_BF_NMDS, Study_Year, kind="se", conf=0.95, lwd=2, col="bisque3", show.groups = "2"))
 
+#biofilms by month
+ordiplot(HC_16S_BF_NMDS, type="n", main="16S Biofilms Month")
+with(HC_16S_BF_NMDS, points(HC_16S_BF_NMDS, display="sites", col=six_col_vec[Days_Carcass_BF], pch=19, pt.bg=six_col_vec))
+with(HC_16S_BF_NMDS, legend("topleft", legend=levels(Days_Carcass_BF), bty="n", col=six_col_vec, pch=19, pt.bg=six_col_vec))
 
 #biofilms permanova
 adonis(HC_16S_com_BF ~ Reach + Date + Reach*Date, data=HC_16S_env_BF, permutations=999)
 
 #make nmds for insects
 HC_16S_I_NMDS<-metaMDS(HC_16S_com_I, distance="bray")
-#Insufficient data right now, so run once more samples
 
+#Visualize reach for insects
+ordiplot(HC_16S_I_NMDS, type="n", main="16S Insect")
+with(HC_16S_I_NMDS, points(HC_16S_I_NMDS, display="sites", col=two_col_vec[stream_site_I], pch=19, pt.bg=two_col_vec))
+
+#permanova insects
+adonis(HC_16S_com_I ~ stream_site_I*Taxa_I, data=HC_16S_env_I, permutations=999)
+
+#nmds carcass before and after biofilms
+HC_16S_CBF_NMDS<-metaMDS(HC_16S_com_CBF, distance="bray")
+
+#nmds plot carcass vs before vs after
+ordiplot(HC_16S_CBF_NMDS, type="n", main="16S Carcass and Biofilm")
+with(HC_16S_CBF_NMDS, points(HC_16S_CBF_NMDS, display="sites", col=three_col_vec[carcass], pch=19, pt.bg=three_col_vec))
+with(HC_16S_CBF_NMDS, legend("topleft", legend=levels(carcass), bty="n", col=three_col_vec, pch=19, pt.bg=three_col_vec))
+with(HC_16S_CBF_NMDS, ordiellipse(HC_16S_CBF_NMDS, carcass, kind="se", conf=0.95, lwd=2, col="#a6cee3", show.groups = "Biofilm After"))
+with(HC_16S_CBF_NMDS, ordiellipse(HC_16S_CBF_NMDS, carcass, kind="se", conf=0.95, lwd=2, col="#1f78b4", show.groups = "Biofilm Before"))
+with(HC_16S_CBF_NMDS, ordiellipse(HC_16S_CBF_NMDS, carcass, kind="se", conf=0.95, lwd=2, col="#b2df8a", show.groups = "Carcass"))
 
 ####################################################################
 ####################################################################
@@ -227,4 +344,40 @@ P_totals<-rbind(HC_16S_P_t, colSums(HC_16S_P_t))
 P_totals<-P_totals[-c(1:96),]
 sort(P_totals,decreasing=TRUE)[1:6]
 rowSums(P_totals)
+
+#limit phyla data to biofilms
+HC_16S_P_map_BF<-subset(HC_16S_P_map, Source == "Biofilm")
+
+#Make line plot for cyanobacteria for salmon vs control
+x<-as.vector((HC_16S_P_map_BF$Total_Biofilm_Growth))
+sum(HC_16S_P_map_BF$"k__Bacteria.p__Cyanobacteria")
+HC_16S_P_map_BF$Cyanobacteria<-as.numeric((HC_16S_P_map_BF$"k__Bacteria.p__Cyanobacteria")/sum(HC_16S_P_map_BF$"k__Bacteria.p__Cyanobacteria"))
+HC_16S_P_map_BF$Reach<-as.factor(HC_16S_P_map_BF$Reach)
+Cyo_p<-data.frame(HC_16S_P_map_BF[,c(8,11,89)])
+tgc <- summarySE(Cyo_p, measurevar="Cyanobacteria", groupvars=c("Total_Biofilm_Growth","Reach"))
+gd <- data.frame(Cyo_p %>% 
+  group_by(Reach, Total_Biofilm_Growth) %>% 
+  summarise(Cyanobacteria = mean(Cyanobacteria)))
+ggplot(tgc, aes(x=Total_Biofilm_Growth, y=Cyanobacteria, colour=Reach)) + 
+  geom_errorbar(aes(ymin=Cyanobacteria-se, ymax=Cyanobacteria+se), width=.1) +
+  geom_line() +
+  geom_point()
+  
+#melt to long format
+names(HC_16S_P_map[,1:14])
+str(HC_16S_P_map[,1:14])
+HC_16S_P_map$Row.names<-as.factor(HC_16S_P_map$Row.names)
+HC_16S_P_map$Month<-as.factor(HC_16S_P_map$Month)
+HC_16S_P_map$Day<-as.factor(HC_16S_P_map$Day)
+HC_16S_P_map$YYYY<-as.factor(HC_16S_P_map$YYYY)
+HC_16S_P_map$Total_Biofilm_Growth<-as.factor(HC_16S_P_map$Total_Biofilm_Growth)
+HC_16S_P_map$Total_Biofilm_Growth_PostCarcass<-as.factor(HC_16S_P_map$Total_Biofilm_Growth_PostCarcass)
+HC_16S_P_map$Year<-as.factor(HC_16S_P_map$Year)
+HC_16S_P_map_m<-melt(HC_16S_P_map, 
+    id.vars=c("Row.names", "BarcodeSequence", "LinkerPrimerSequence", "Month", "Day", "YYYY", "Date", "Reach", "Subreach", "Source", "Total_Biofilm_Growth", "Total_Biofilm_Growth_PostCarcass", "Year", "Description"))
+
+#Delete zeros in melted file
+HC_16S_P_map_m<-subset(HC_16S_P_map_m, value > 0)
+
+
 
--- Hunt_Creek_Macroinvertebrate.R
+++ Hunt_Creek_Macroinvertebrate.R
@@ -4,6 +4,54 @@
 library(vegan)
 library(reshape)
 library(ggplot2)
+library(plyr)
+library(dplyr)
+library(doBy)
+
+#Functions
+## Summarizes data.
+## Gives count, mean, standard deviation, standard error of the mean, and confidence 
+## interval (default 95%).
+##   data: a data frame.
+##   measurevar: the name of a column that contains the variable to be summariezed
+##   groupvars: a vector containing names of columns that contain grouping variables
+##   na.rm: a boolean that indicates whether to ignore NA's
+##   conf.interval: the percent range of the confidence interval (default is 95%)
+summarySE <- function(data=NULL, measurevar, groupvars=NULL, na.rm=FALSE, conf.interval=.95) {
+  library(doBy)
+  
+  # New version of length which can handle NA's: if na.rm==T, don't count them
+  length2 <- function (x, na.rm=FALSE) {
+    if (na.rm) sum(!is.na(x))
+    else       length(x)
+  }
+  
+  # Collapse the data
+  formula <- as.formula(paste(measurevar, paste(groupvars, collapse=" + "), sep=" ~ "))
+  datac <- summaryBy(formula, data=data, FUN=c(length2,mean,sd), na.rm=na.rm)
+  
+  # Rename columns
+  names(datac)[ names(datac) == paste(measurevar, ".mean",    sep="") ] <- measurevar
+  names(datac)[ names(datac) == paste(measurevar, ".sd",      sep="") ] <- "sd"
+  names(datac)[ names(datac) == paste(measurevar, ".length2", sep="") ] <- "N"
+  
+  datac$se <- datac$sd / sqrt(datac$N)  # Calculate standard error of the mean
+  
+  # Confidence interval multiplier for standard error
+  # Calculate t-statistic for confidence interval: 
+  # e.g., if conf.interval is .95, use .975 (above/below), and use df=N-1
+  ciMult <- qt(conf.interval/2 + .5, datac$N-1)
+  datac$ci <- datac$se * ciMult
+  
+  return(datac)
+}
+
+#Make color vectors
+two_col_vec <- c("black", "bisque3")
+two_col_vec_ob<-c("#ef6548", "#3690c0")
+eight_col_vec<-c("#f7fcfd", "#e5f5f9", "#ccece6", "#99d8c9", "#66c2a4", "#41ae76", "#238b45", "#005824")
+eleven_col_vec <- c("gray0", "gray9", "gray18", "gray27", "gray36", "gray45", "gray54", "gray63", "gray72", "gray81", "gray90")
+fifteen_col_vec<-c("#fee8c8", "#fff7fb", "#fdd49e", "#ece7f2", "#d0d1e6", "#fdbb84", "#a6bddb", "#fc8d59", "#74a9cf", "#ef6548" ,"#3690c0", "#d7301f", "#0570b0", "#990000", "#034e7b")
 
 #Upload .csv file
 Hunt_Creek_Macroinvertebrates<-read.csv("~/Documents/MSU/Research/Hunt_Creek_Salmon/Macroinvertebrates/Hunt_Creek_Macroinvertebrates.csv", sep = ",", header = T )
@@ -43,36 +91,123 @@ str(HC_M_Matrix$Days_Since_Carcass_Introduction)
 Days_Since_Introduction_n=as.numeric(HC_Inverts_Env$Days_Since_Carcass_Introduction)
 Days_Since_Introduction_f=factor(HC_Inverts_Env$Days_Since_Carcass_Introduction)
 levels(Days_Since_Introduction_f)
-
-#Months since introduction - not done yet
-Months_Since_Introduction<-factor(Days_Since_Introduction)
-Months_Since_Introduction<-gsub(16, 1, Months_Since_Introduction)
-levels(Months_Since_Introduction)
+Days_Since_Introduction_f<-as.factor(gsub("161", "162", Days_Since_Introduction_f))
+Days_Since_Introduction_f<-as.factor(gsub("265", "264", Days_Since_Introduction_f))
+Days_Since_Introduction_f<-as.factor(gsub("312", "306", Days_Since_Introduction_f))
+Days_Carcass<-factor(Days_Since_Introduction_f, c("0", "1", "16", "162", "222", "264", "306", "352"))
 
 #Set up factor for Days_Since_Carcass_Introduction_f_stream_site
 D_S_C_I_f_s_s <- factor(paste(HC_Inverts_Env$Days_Since_Carcass_Introduction, HC_Inverts_Env$Reach))
 levels(D_S_C_I_f_s_s)
+D_S_C_I_f_s_s<-as.factor(gsub("161 Control", "162 Control", D_S_C_I_f_s_s))
+D_S_C_I_f_s_s<-as.factor(gsub("161 Salmon", "162 Salmon", D_S_C_I_f_s_s))
+D_S_C_I_f_s_s<-as.factor(gsub("265 Salmon", "264 Salmon", D_S_C_I_f_s_s))
+D_S_C_I_f_s_s<-as.factor(gsub("265 Control", "264 Control", D_S_C_I_f_s_s))
+D_S_C_I_f_s_s<-as.factor(gsub("312 Control", "306 Control", D_S_C_I_f_s_s))
+D_S_C_I_f_s_s<-as.factor(gsub("312 Salmon", "306 Salmon", D_S_C_I_f_s_s))
+Days_Carcass_Reach<-factor(D_S_C_I_f_s_s, c("0 Control", "0 Salmon", "1 Control", "1 Salmon", "16 Salmon", "162 Control", "162 Salmon", "222 Control", "222 Salmon", "264 Control", "264 Salmon", "306 Control", "306 Salmon", "352 Control", "352 Salmon"))
+
+#make factor for year
+Year<-factor(HC_Inverts_Env$Date)
+levels(Year)
+Year<-as.factor(gsub("10/25/15", "2", Year))
+Year<-as.factor(gsub("10/4/15", "2", Year))
+Year<-as.factor(gsub("3/19/16", "2", Year))
+Year<-as.factor(gsub("5/17/16", "2", Year))
+Year<-as.factor(gsub("6/30/16", "2", Year))
+Year<-as.factor(gsub("10/4/14", "1", Year))
+Year<-as.factor(gsub("3/12/15", "1", Year))
+Year<-as.factor(gsub("3/19/15", "2", Year)) #typo in data entry will fix master later
+Year<-as.factor(gsub("5/12/15", "1", Year))
+Year<-as.factor(gsub("6/23/15", "1", Year))
+Year<-as.factor(gsub("8/4/15", "1", Year))
+Year<-as.factor(gsub("9/20/15", "1", Year))
+Year<-as.factor(gsub("9/5/14", "1", Year))
+Year<-as.factor(gsub("8/15/16", "2", Year))
 
 #NMDS analysis
 HC_Macroinvertebrate_NMDS<-metaMDS(HC_Inverts_Community, distance="bray")
-ordiplot(HC_Macroinvertebrate_NMDS, type="n", main="Invertebrate")
 
-#Make color schemes
-two_col_vec <- c("black", "bisque3")
-eleven_col_vec <- c("gray0", "gray9", "gray18", "gray27", "gray36", "gray45", "gray54", "gray63", "gray72", "gray81", "gray90")
-  
+#Stressplot macroinvertebrate Nmds
+stressplot(HC_Macroinvertebrate_NMDS)
+
 #NMDS plot for stream_site Salmon vs Control
+ordiplot(HC_Macroinvertebrate_NMDS, type="n", main="Invertebrate")
 with(HC_Macroinvertebrate_NMDS, points(HC_Macroinvertebrate_NMDS, display="sites", col=two_col_vec[stream_site], pch=19, pt.bg=two_col_vec))
 with(HC_Macroinvertebrate_NMDS, legend("topleft", legend=levels(stream_site), bty="n", col=two_col_vec, pch=19, pt.bg=two_col_vec))
 with(HC_Macroinvertebrate_NMDS, ordiellipse(HC_Macroinvertebrate_NMDS, stream_site, kind="se", conf=0.95, lwd=2, col="black", show.groups = "Salmon"))
 with(HC_Macroinvertebrate_NMDS, ordiellipse(HC_Macroinvertebrate_NMDS, stream_site, kind="se", conf=0.95, lwd=2, col="bisque3", show.groups = "Control"))
 
 #NMDS plot for Days_Since_Introduction_f
-with(HC_Macroinvertebrate_NMDS, points(HC_Macroinvertebrate_NMDS, display="sites", col=eleven_col_vec[Days_Since_Introduction_f], pch=19, pt.bg=eleven_col_vec))
+ordiplot(HC_Macroinvertebrate_NMDS, type="n", main="Macroinvertebrate Days since introduction")
+with(HC_Macroinvertebrate_NMDS, points(HC_Macroinvertebrate_NMDS, display="sites", col=eight_col_vec[Days_Carcass], pch=19, pt.bg=eight_col_vec))
+with(HC_Macroinvertebrate_NMDS, legend("topleft", legend=levels(Days_Carcass), bty="n", col=eight_col_vec, pch=19, pt.bg=eight_col_vec))
+with(HC_Macroinvertebrate_NMDS, ordiellipse(HC_Macroinvertebrate_NMDS, Days_Carcass, kind="se", conf=0.95, lwd=2, col="#f7fcfd", show.groups = "0"))
+with(HC_Macroinvertebrate_NMDS, ordiellipse(HC_Macroinvertebrate_NMDS, Days_Carcass, kind="se", conf=0.95, lwd=2, col="#e5f5f9", show.groups = "1"))
+with(HC_Macroinvertebrate_NMDS, ordiellipse(HC_Macroinvertebrate_NMDS, Days_Carcass, kind="se", conf=0.95, lwd=2, col="#ccece6", show.groups = "16"))
+with(HC_Macroinvertebrate_NMDS, ordiellipse(HC_Macroinvertebrate_NMDS, Days_Carcass, kind="se", conf=0.95, lwd=2, col="#99d8c9", show.groups = "162"))
+with(HC_Macroinvertebrate_NMDS, ordiellipse(HC_Macroinvertebrate_NMDS, Days_Carcass, kind="se", conf=0.95, lwd=2, col="#66c2a4", show.groups = "222"))
+with(HC_Macroinvertebrate_NMDS, ordiellipse(HC_Macroinvertebrate_NMDS, Days_Carcass, kind="se", conf=0.95, lwd=2, col="#41ae76", show.groups = "264"))
+with(HC_Macroinvertebrate_NMDS, ordiellipse(HC_Macroinvertebrate_NMDS, Days_Carcass, kind="se", conf=0.95, lwd=2, col="#238b45", show.groups = "306"))
+with(HC_Macroinvertebrate_NMDS, ordiellipse(HC_Macroinvertebrate_NMDS, Days_Carcass, kind="se", conf=0.95, lwd=2, col="#005824", show.groups = "352"))
 
-#NMDS plot for Days_Since_Introction_f_stream_site
+#NMDS plot by year
+ordiplot(HC_Macroinvertebrate_NMDS, type="n", main="Macroinvertebrate Days since introduction")
+with(HC_Macroinvertebrate_NMDS, points(HC_Macroinvertebrate_NMDS, display="sites", col=two_col_vec[Year], pch=19, pt.bg=two_col_vec))
+with(HC_Macroinvertebrate_NMDS, legend("topleft", legend=levels(Year), bty="n", col=two_col_vec, pch=19, pt.bg=two_col_vec))
+
+
+#NMDS plot for days carcass reach interaction
+ordiplot(HC_Macroinvertebrate_NMDS, type="n", main="Macroinvertebrate Days since introduction Reach")
+with(HC_Macroinvertebrate_NMDS, points(HC_Macroinvertebrate_NMDS, display="sites", col=fifteen_col_vec[Days_Carcass_Reach], pch=19, pt.bg=fifteen_col_vec))
+with(HC_Macroinvertebrate_NMDS, legend("topleft", legend=levels(stream_site), bty="n", col=two_col_vec_ob, pch=19, pt.bg=two_col_vec_ob))
+with(HC_Macroinvertebrate_NMDS, ordiellipse(HC_Macroinvertebrate_NMDS, Year, kind="se", conf=0.95, lwd=2, col="black", show.groups = "1"))
+with(HC_Macroinvertebrate_NMDS, ordiellipse(HC_Macroinvertebrate_NMDS, Year, kind="se", conf=0.95, lwd=2, col="bisque3", show.groups = "2"))
 
 #Permanova for stream_site Salmon vs. Control
-adonis(HC_Inverts_Community ~ stream_site, data=HC_Inverts_Env, permutations=999)
+adonis(HC_Inverts_Community ~ stream_site*Days_Carcass*Year, data=HC_Inverts_Env, permutations=999)
 
+#############
+#Now on to populations rather than communities
 
+#find top taxa
+HC_Inverts_Community<-HC_M_Matrix[,7:ncol(HC_M_Matrix)]
+HC_Inverts_Community[is.na(HC_Inverts_Community)]<-0
+totals<-rbind(HC_Inverts_Community, colSums(HC_Inverts_Community))
+totals<-totals[-c(1:105),]
+rowSums(totals)
+sort(totals,decreasing=TRUE)[1:6]
+
+#Make line plot for Baetids
+HC_M_Matrix[,7:ncol(HC_M_Matrix)][is.na(HC_M_Matrix[,7:ncol(HC_M_Matrix)])]<-0
+HC_M_Matrix$RowSums<-rowSums(HC_M_Matrix[,7:ncol(HC_M_Matrix)])
+HC_M_Matrix$Baetidae<-(HC_M_Matrix$"Hexapoda Ephemeroptera Baetidae"/HC_M_Matrix$RowSums)
+Baetidaeplot<-data.frame(HC_M_Matrix[,c(3,6,53)])
+Baetidaeplot[is.na(Baetidaeplot)]<-0
+str(Baetidaeplot)
+Baetidaeplot$Days_Since_Carcass_Introduction<-as.numeric(Baetidaeplot$Days_Since_Carcass_Introduction)
+tgc<-summarySE(Baetidaeplot, measurevar="Baetidae", groupvars=c("Days_Since_Carcass_Introduction", "Reach"))
+ggplot(tgc, aes(x=Days_Since_Carcass_Introduction, y=Baetidae, colour=Reach)) + 
+  geom_errorbar(aes(ymin=Baetidae-se, ymax=Baetidae+se), width=.1) +
+  geom_line() +
+  geom_point()
+
+#Make line plot for Simuliids
+HC_M_Matrix$Simuliidae<-(HC_M_Matrix$"Hexapoda Diptera Simuliidae"/HC_M_Matrix$RowSums)
+Simuliidplot<-data.frame(HC_M_Matrix[,c(3,6,54)])
+Simuliidplot$Days_Since_Carcass_Introduction<-as.numeric(Simuliidplot$Days_Since_Carcass_Introduction)
+tgs<-summarySE(Simuliidplot, measurevar="Simuliidae", groupvars=c("Days_Since_Carcass_Introduction", "Reach"))
+ggplot(tgs, aes(x=Days_Since_Carcass_Introduction, y=Simuliidae, colour=Reach)) + 
+  geom_errorbar(aes(ymin=Simuliidae-se, ymax=Simuliidae+se), width=.1) +
+  geom_line() +
+  geom_point()
+
+#Make line plot for Rhyacophila
+HC_M_Matrix$Rhyacophila<-(HC_M_Matrix$"Hexapoda Trichoptera Rhyacophila"/HC_M_Matrix$RowSums)
+Rhyaplot<-data.frame(HC_M_Matrix[,c(3,6,55)])
+Rhyaplot$Days_Since_Carcass_Introduction<-as.numeric(Rhyaplot$Days_Since_Carcass_Introduction)
+tgr<-summarySE(Rhyaplot, measurevar="Rhyacophila", groupvars=c("Days_Since_Carcass_Introduction", "Reach"))
+ggplot(tgr, aes(x=Days_Since_Carcass_Introduction, y=Rhyacophila, colour=Reach)) + 
+  geom_errorbar(aes(ymin=Rhyacophila-se, ymax=Rhyacophila+se), width=.1) +
+  geom_line() +
+  geom_point()
